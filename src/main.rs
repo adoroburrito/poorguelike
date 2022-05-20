632f903b1bfd47e604555aa39f4dfeb45b8bdf2e
André Nogueira <nog@naya.local>
2022-05-19 23:43:37 -0300

    Starting over. This commit: WIP World Generation - Create vector of room structs that are meant to be sequentially (staircase logic WIP) connected - Rooms have tiles that hold only references to other important data located in that file (terrain, actors, entities and if there is a staircase in that spot)
    To-do:
    - Finish staircase logic
    - Add sprite references for terrain, entities and actors
    - Start work on rendering, tackling viewport into the room and moving it alongside the player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+use rand::Rng;
+use std::collections::HashMap;
+
 use raylib::color::Color;
 use raylib::prelude::*;
+
+#[derive(Debug)]
+enum StairsDirection {
+    UP,
+    DOWN,
+}
+
+#[derive(Eq, PartialEq, Hash, Debug)]
+enum TerrainVariant {
+    Dirt1(Terrain),
+    Dirt2(Terrain),
+    Grass1(Terrain),
+    Grass2(Terrain),
+    BrickWall1(Terrain),
+    BrickWall2(Terrain),
+}
+
+#[derive(Eq, PartialEq, Hash)]
+enum TerrainName {
+    Dirt1,
+    Dirt2,
+    Grass1,
+    Grass2,
+    BrickWall1,
+    BrickWall2,
+}
 
 struct GraphicSettings {
     tile_size: u32,
@@ -9,89 +38,186 @@ struct WindowSettings {
     height: f32,
     width: f32,
 }
-pub struct Settings {
+struct Settings {
     graphic: GraphicSettings,
     window: WindowSettings,
 }
 
-pub fn draw_grid(d: &mut RaylibDrawHandle, settings: &Settings) {
-    // based on the tile size:
-    // how many tiles fit horizontally and vertically?
-    let tile_size_f32: f32 = settings.graphic.tile_size as f32;
-    let target_tile_quantity_horizontally: u32 =
-        (settings.window.width / tile_size_f32).ceil() as u32;
-    let target_tile_quantity_vertically: u32 =
-        (settings.window.height / tile_size_f32).ceil() as u32;
+#[derive(Eq, PartialEq, Hash, Debug)]
+struct Face {}
 
-    for n in 0..=target_tile_quantity_horizontally {
-        d.draw_line(
-            (n * settings.graphic.tile_size).try_into().unwrap(),
-            0,
-            (n * settings.graphic.tile_size).try_into().unwrap(),
-            settings.window.height as i32,
-            Color::from_hex("AAAAAA").unwrap(),
-        );
-    }
-
-    for n in 0..target_tile_quantity_vertically {
-        d.draw_line(
-            0,
-            (n * settings.graphic.tile_size).try_into().unwrap(),
-            settings.window.width as i32,
-            (n * settings.graphic.tile_size).try_into().unwrap(),
-            Color::from_hex("AAAAAA").unwrap(),
-        );
-    }
+#[derive(Eq, PartialEq, Hash, Debug)]
+struct Terrain {
+    traversable: bool,
+    face: Face,
 }
 
-pub fn draw_static_tile_positions(d: &mut RaylibDrawHandle, settings: &Settings) {
-    // based on the tile size:
-    // how many tiles fit horizontally and vertically?
-    let tile_size_f32: f32 = settings.graphic.tile_size as f32;
-    let target_tile_quantity_horizontally: u32 =
-        (settings.window.width / tile_size_f32).ceil() as u32;
-    let target_tile_quantity_vertically: u32 =
-        (settings.window.height / tile_size_f32).ceil() as u32;
+#[derive(Debug)]
+struct Entity {
+    pos_x: usize,
+    pos_y: usize,
+    face: Face,
+}
 
-    let padding = 5;
+#[derive(Debug)]
+struct Actor {
+    pos_x: usize,
+    pos_y: usize,
+    face: Face,
+}
 
-    for x in 0..=target_tile_quantity_horizontally {
-        for y in 0..=target_tile_quantity_vertically {
-            d.draw_text(
-                &format!("X: {} \nY: {}", x, y),
-                ((x * settings.graphic.tile_size) + padding) as i32,
-                ((y * settings.graphic.tile_size) + padding) as i32,
-                8,
-                Color::WHITE,
-            );
+#[derive(Debug)]
+struct RoomTile<'a> {
+    stairs: Option<&'a StairsDirection>,
+    terrain: &'a TerrainVariant,
+    entities: Vec<&'a Entity>,
+    actors: Vec<&'a Actor>,
+}
+
+#[derive(Debug)]
+struct Room<'a> {
+    tiles: Vec<RoomTile<'a>>,
+    size: UVec2D,
+}
+
+#[derive(Debug)]
+struct UVec2D {
+    x: usize,
+    y: usize,
+}
+
+fn make_room(size: UVec2D) -> Room<'static> {
+    let size_x = size.x;
+    let size_y = size.y;
+
+    let mut room_tiles: Vec<RoomTile> = Vec::new();
+
+    // generate tiles for simple square room with random terrain
+    for x in 1..=size_x {
+        for y in 1..=size_y {
+            let wall_x = match x {
+                1 => true,
+                size_x => true,
+                _ => false,
+            };
+
+            let wall_y = match y {
+                1 => true,
+                size_y => true,
+                _ => false,
+            };
+
+            let wall = wall_x || wall_y;
+
+            let mut rng = rand::thread_rng();
+
+            let variant_name: &TerrainName = match wall {
+                true => {
+                    // TO-DO: for now there are only 2 walls, this needs to be incremented for when there
+                    // are more types of walls/more variants
+                    let wall_rng = rng.gen_range(0..2);
+
+                    match wall_rng {
+                        0 => &TerrainName::BrickWall1,
+                        1 => &TerrainName::BrickWall2,
+                        _ => unreachable!(),
+                    }
+                }
+                false => {
+                    // TO-DO: for now there are only 4 ground variants, this needs to be incremented for when there
+                    // are more variants
+                    let ground_rng = rng.gen_range(0..4);
+
+                    match ground_rng {
+                        0 => &TerrainName::Dirt1,
+                        1 => &TerrainName::Dirt2,
+                        2 => &TerrainName::Grass1,
+                        3 => &TerrainName::Grass2,
+                        _ => unreachable!(),
+                    }
+                }
+            };
+
+            let terrain: &TerrainVariant = match variant_name {
+                &TerrainName::Dirt1 => &ALL_TERRAINS[0],
+                &TerrainName::Dirt2 => &ALL_TERRAINS[1],
+                &TerrainName::Grass1 => &ALL_TERRAINS[2],
+                &TerrainName::Grass2 => &ALL_TERRAINS[3],
+                &TerrainName::BrickWall1 => &ALL_TERRAINS[4],
+                &TerrainName::BrickWall2 => &ALL_TERRAINS[5],
+            };
+
+            let entities: Vec<&Entity> = Vec::new();
+            let actors: Vec<&Actor> = Vec::new();
+
+            room_tiles.push(RoomTile {
+                stairs: None,
+                terrain,
+                entities,
+                actors,
+            })
         }
     }
-}
 
-pub fn draw_world_tile_positions(d: &mut RaylibDrawHandle, settings: &Settings) {
-    // based on the tile size:
-    // how many tiles fit horizontally and vertically?
-    let tile_size_f32: f32 = settings.graphic.tile_size as f32;
-    let target_tile_quantity_horizontally: u32 =
-        (settings.window.width / tile_size_f32).ceil() as u32;
-    let target_tile_quantity_vertically: u32 =
-        (settings.window.height / tile_size_f32).ceil() as u32;
-
-    let padding_x = 40;
-    let padding_y = 35;
-
-    for x in 0..=target_tile_quantity_horizontally {
-        for y in 0..=target_tile_quantity_vertically {
-            d.draw_text(
-                &format!("X: {} \nY: {}", x, y),
-                ((x * settings.graphic.tile_size) + padding_x) as i32,
-                ((y * settings.graphic.tile_size) + padding_y) as i32,
-                8,
-                Color::RED,
-            );
-        }
+    Room {
+        tiles: room_tiles,
+        size: UVec2D {
+            x: size_x,
+            y: size_y,
+        },
     }
 }
+
+fn place_staircases(room: &mut Room, entrance: bool, exit: bool) {
+    let x = room.size.x;
+    let y = room.size.y;
+}
+
+fn prepare_world(rooms: usize) -> Vec<Room<'static>> {
+    println!("Generating world with {} rooms...", rooms);
+    let mut world: Vec<Room> = Vec::new();
+    let last_room: usize = rooms - 1;
+
+    for n in 0..rooms {
+        println!("Generating room n #{}", n + 1);
+        let mut room = make_room(UVec2D { x: 1000, y: 1000 });
+        match n {
+            0 => place_staircases(&mut room, false, true),
+            last_room => place_staircases(&mut room, true, false),
+            _ => place_staircases(&mut room, true, true),
+        }
+        world.push(room);
+    }
+
+    world
+}
+
+const ALL_TERRAINS: [TerrainVariant; 6] = [
+    TerrainVariant::Dirt1(Terrain {
+        traversable: true,
+        face: Face {},
+    }),
+    TerrainVariant::Dirt2(Terrain {
+        traversable: true,
+        face: Face {},
+    }),
+    TerrainVariant::Grass1(Terrain {
+        traversable: true,
+        face: Face {},
+    }),
+    TerrainVariant::Grass2(Terrain {
+        traversable: true,
+        face: Face {},
+    }),
+    TerrainVariant::BrickWall1(Terrain {
+        traversable: false,
+        face: Face {},
+    }),
+    TerrainVariant::BrickWall2(Terrain {
+        traversable: false,
+        face: Face {},
+    }),
+];
 
 fn main() {
     let window_height = 640.0;
@@ -111,12 +237,11 @@ fn main() {
         },
     };
 
+    let world = prepare_world(10);
+    dbg!(&world[0]);
+
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
-
-        draw_grid(&mut d, &settings);
-        draw_static_tile_positions(&mut d, &settings);
-        draw_world_tile_positions(&mut d, &settings);
     }
 }
